d60b426567b917a604ae1db626400d9789c600b6
Yashraj4208 <166053468+Yashraj4208@users.noreply.github.com>
2024-12-09 21:54:36 +0530

    Delete session1.php

--- session1.php
+++
@@ -1,7 +0,0 @@
-session1.php 
-<?php 
-session_start(); 
-echo "<h3> session variables that were set on previous page are</h3> <br>"; 
-echo "Favorite color is " . $_SESSION["favcolor"]."<br>"; 
-echo "Favorite animal is " . $_SESSION["favanimal"]; 
-?>
